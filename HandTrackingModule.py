055156f32f077a48fbc6b937c81d3958e8c56dcf
Waltinho <116035407+WaltinhoCan@users.noreply.github.com>
2022-10-24 16:53:20 -0300

    Update HandTrackingModule.py

--- HandTrackingModule.py
+++ HandTrackingModule.py
@@ -1,6 +1,5 @@
 '''
-Hand tracking Module
-Name: Syed
+Walter Guilherme Candeia
 '''
 
 
@@ -8,55 +7,52 @@ import cv2
 import mediapipe as mp
 import time
 
+class Detector():
+    def __init__(self, modo = False, NumMaos = 1, CtzDeteccao = 0.5, CtzMapeamento = 0.5):
+        self.modo = modo
+        self.NumMaos = NumMaos
+        self.CtzDeteccao = CtzDeteccao
+        self.CtzMapeamento = CtzMapeamento
 
-class handDetector():
-    def __init__(self, mode = False, maxHands = 2, detectionCon = 0.5, trackCon = 0.5):
-        self.mode = mode
-        self.maxHands = maxHands
-        self.detectionCon = detectionCon
-        self.trackCon = trackCon
+        self.mpMaos = mp.solutions.hands
+        self.Maos = self.mpMaos.Hands(self.modo, self.NumMaos, self.CtzDeteccao, self.CtzMapeamento)
+        self.mpDesenho = mp.solutions.drawing_utils
 
-        self.mpHands = mp.solutions.hands
-        self.hands = self.mpHands.Hands(self.mode, self.maxHands, self.detectionCon, self.trackCon)
-        self.mpDraw = mp.solutions.drawing_utils
-        
-    def findHands(self,img, draw = True):
-        imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-        self.results = self.hands.process(imgRGB)
-        print(results.multi_hand_landmarks)
+    def EncontPosi(self, img, SMao = 0, draw = True):
 
+        Ptlist = []
         if self.results.multi_hand_landmarks:
-            for handLms in self.results.multi_hand_landmarks:
-                if draw:
-                    self.mpDraw.draw_landmarks(img, handLms, self.mpHands.HAND_CONNECTIONS)
-        return img
-
-    def findPosition(self, img, handNo = 0, draw = True):
-
-        lmlist = []
-        if self.results.multi_hand_landmarks:
-            myHand = self.results.multi_hand_landmarks[handNo]
-            for id, lm in enumerate(myHand.landmark):
+            myMao = self.results.multi_hand_landmarks[SMao]
+            for id, pt in enumerate(myMao.landmark):
                 h, w, c = img.shape
-                cx, cy = int(lm.x * w), int(lm.y * h)
-                lmlist.append([id, cx, cy])
+                cx, cy = int(pt.x * w), int(pt.y * h)
+                Ptlist.append([id, cx, cy])
                 if draw:
                     cv2.circle(img, (cx, cy), 7, (255, 0, 255), cv2.FILLED)
-        return lmlist
+        return Ptlist
 
+    def EncontMaos(self,img, draw = True):
+        imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        self.results = self.Maos.process(imgRGB)
 
+        if self.results.multi_hand_landmarks:
+            for maospts in self.results.multi_hand_landmarks:
+                if draw:
+                    self.mpDesenho.draw_landmarks(img, maospts, self.mpMaos.HAND_CONNECTIONS)
+        return img
+    
 def main():
     pTime = 0
     cTime = 0
     cap = cv2.VideoCapture(0)
-    detector = handDetector()
+    detector = Detector()
 
     while True:
         success, img = cap.read()
-        img = detector.findHands(img)
-        lmlist = detector.findPosition(img)
-        if len(lmlist) != 0:
-            print(lmlist[4])
+        img = detector.EncontMaos(img)
+        Ptlist = detector.EncontPosi(img)
+        if len(Ptlist) != 0:
+            print(Ptlist[4])
 
         cTime = time.time()
         fps = 1 / (cTime - pTime)
@@ -66,6 +62,7 @@ def main():
 
         cv2.imshow("Image", img)
         cv2.waitKey(1)
+
 
 
 if __name__ == "__main__":
